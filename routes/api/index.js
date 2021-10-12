403b3ef9ae5ed18f8203f402f641834dc3c82868
Islam Akhrarov <skyart450@gmail.com>
2021-10-13 04:33:01 +0500

    Routes full setup

--- routes/api/index.js
+++ routes/api/index.js
@@ -5,6 +5,9 @@ const authRoute = require("./auth");
 const coursesRoute = require("./courses");
 const usersRoute = require("./users");
 
+router.get("/", (req, res) => {
+  res.send("This is API route");
+});
 router.use("/auth", authRoute);
 router.use("/courses", coursesRoute);
 router.use("/users", usersRoute);
--- routes/index.js
+++ routes/index.js
@@ -3,5 +3,13 @@ const router = express.Router();
 
 const apiRoute = require("./api/");
 
+router.get("/", (req, res) => {
+  res.send("Welcome");
+});
+
 router.use("/api", apiRoute);
+router.get("*", (req, res) => {
+  res.status(404).json({ message: "Page not found" });
+});
+
 module.exports = router;
