bfe62d59fa29a8e10ba6d7ac85dc7708ba5861fd
Islam Akhrarov <skyart450@gmail.com>
2021-10-13 04:34:53 +0500

    routes full setup

--- app.js
+++ app.js
@@ -2,12 +2,11 @@ const express = require("express");
 const app = express();
 
 app.set("view engine", "ejs");
-// app.use(express.static(public));
+app.use(express.static("public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-const routes = require("./routes/index");
-app.use(routes);
+app.use(require("./routes/index")); //initializing routes
 
 app.listen(
   process.env.PORT || 3000,
